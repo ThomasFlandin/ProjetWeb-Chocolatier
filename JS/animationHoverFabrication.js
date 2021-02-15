c52ce91f2e03ce9c647723a47bcbcd2579216686
Thomas <thomas.fla@outlook.fr>
2021-02-15 12:34:29 +0100

    MAJ remplacement Css par Js pour animation Fabrication + Valisations W3C

---
+++ JS/animationHoverFabrication.js
@@ -0,0 +1,23 @@
+var parents = document.getElementsByClassName('parent');
+
+for(var i = 0 ; i < parents.length; i++)
+{
+    var image = parents[i].getElementsByTagName("img")[0];
+    image.addEventListener('mouseover', animationHover);
+}
+
+
+function animationHover()
+{
+    //
+    enfants = document.getElementsByClassName("enfant");
+    for(var i=0; i < enfants.length; i++)
+    {
+        enfants[i].style.opacity = "0%";
+    }
+
+    var parent = this.parentNode;
+    var enfant = parent.getElementsByClassName('enfant')[0];
+    enfant.style.transition = "0.6s";
+    enfant.style.opacity = "100%";
+}
