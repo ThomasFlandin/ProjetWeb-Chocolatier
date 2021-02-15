01bcee05a02fe76c63f01b7393217e845bb24dfc
Thomas <thomas.fla@outlook.fr>
2021-02-15 14:18:00 +0100

    Optimisation balises Html + taille texte dans fabrication.html

--- JS/animationHoverFabrication.js
+++ JS/animationHoverFabrication.js
@@ -14,10 +14,12 @@ function animationHover()
     for(var i=0; i < enfants.length; i++)
     {
         enfants[i].style.opacity = "0%";
+        enfants[i].style.zIndex = "auto";
     }
 
     var parent = this.parentNode;
     var enfant = parent.getElementsByClassName('enfant')[0];
     enfant.style.transition = "0.6s";
     enfant.style.opacity = "100%";
+    enfant.style.zIndex = "2";
 }
